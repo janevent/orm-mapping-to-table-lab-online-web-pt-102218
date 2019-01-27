811a5356aeab1e10a10eae2432c2b57917184308
Jane Venter <janeanneventer@gmail.com>
2019-01-27 00:21:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     (name, grade)
     VALUES (?, ?)
     anything
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
     @id =DB[:conn].execute("SELECT last_insert_rowid() FROM students")
     
   end
