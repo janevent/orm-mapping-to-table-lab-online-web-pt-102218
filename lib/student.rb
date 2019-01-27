7b1755d65b35d9cccc00ceb2af555f9825711c10
Jane Venter <janeanneventer@gmail.com>
2019-01-27 00:02:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,9 @@ class Student
     anything
     DB[:conn].execute(sql)
   end
+  
+  def save
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
